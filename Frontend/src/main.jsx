e07305641dedb0a7fa99397c20779e08bc30ac09
SUNAL PAL <sunal@SUNAL.com>
2025-11-27 23:42:09 +0530

    complete the folder Structure of store

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,13 @@ import { createRoot } from 'react-dom/client'
 import "../src/index.css"
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
+import {store}from './store/store.jsx'
+import {Provider} from 'react-redux'
 createRoot(document.getElementById('root')).render(
+
+<Provider store={store}>
 <BrowserRouter>
     <App />
 </BrowserRouter>
+</Provider>
 )
